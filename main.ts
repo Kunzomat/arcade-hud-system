ffa9ec8ff3ad98c3733adfe743b5cf2e7010136d
Andreas Brisgen <abrisgen@intershop.de>
2026-02-13 11:10:49 +0100

    cockpit

--- main.ts
+++ main.ts
@@ -1,53 +1,116 @@
-namespace HUD {
+//% color=#00FFFF icon="\uf108"
+namespace cockpit {
 
-    let ammo = 0
-    let rockets = 0
-    let energy = 0
-    let fuel = 0
-    let points = 0
-    let hudSprite: TextSprite = null
-
-    export function init() {
-        hudSprite = textsprite.create("")
-        hudSprite.setPosition(80, 10)
-        hudSprite.setFlag(SpriteFlag.RelativeToCamera, true)
+    export enum DisplayType {
+        //% block="8 segment number"
+        Number = 0,
+        //% block="bar"
+        Bar = 1,
+        //% block="icons"
+        Icons = 2
     }
 
-    export function setAmmo(value: number) {
-        ammo = value
-        update()
-    }
+    class CockpitElement {
 
-    export function setRockets(value: number) {
-        rockets = value
-        update()
-    }
+        icon: Sprite
+        value = 0
+        maxValue = 10
+        type: DisplayType
 
-    export function setEnergy(value: number) {
-        energy = value
-        update()
-    }
+        textSprite: TextSprite = null
+        bar: StatusBarSprite = null
+        iconSprites: Sprite[] = []
 
-    export function setFuel(value: number) {
-        fuel = value
-        update()
-    }
+        constructor(iconImage: Image, type: DisplayType) {
 
-    export function setPoints(value: number) {
-        points = value
-        update()
-    }
+            this.type = type
 
-    function update() {
-        if (hudSprite) {
-            hudSprite.setText(
-                "A:" + ammo +
-                " R:" + rockets +
-                " E:" + energy +
-                " F:" + fuel +
-                " P:" + points
-            )
+            this.icon = sprites.create(iconImage, SpriteKind.UI)
+            this.icon.setFlag(SpriteFlag.RelativeToCamera, true)
+
+            if (type == DisplayType.Number) {
+                this.textSprite = textsprite.create("0")
+                this.textSprite.setFlag(SpriteFlag.RelativeToCamera, true)
+            }
+
+            if (type == DisplayType.Bar) {
+                this.bar = statusbars.create(30, 6, StatusBarKind.Health)
+                this.bar.setFlag(SpriteFlag.RelativeToCamera, true)
+                this.bar.setMax(this.maxValue)
+            }
+        }
+
+        setPosition(x: number, y: number) {
+
+            this.icon.setPosition(x, y)
+
+            if (this.textSprite)
+                this.textSprite.setPosition(x + 25, y)
+
+            if (this.bar)
+                this.bar.setPosition(x + 35, y)
+
+            this.updateIcons(x, y)
+        }
+
+        setValue(v: number) {
+            this.value = v
+
+            if (this.textSprite)
+                this.textSprite.setText("" + v)
+
+            if (this.bar)
+                this.bar.value = v
+
+            if (this.type == DisplayType.Icons)
+                this.updateIcons(this.icon.x, this.icon.y)
+        }
+
+        setMax(max: number) {
+            this.maxValue = max
+            if (this.bar)
+                this.bar.setMax(max)
+        }
+
+        updateIcons(baseX: number, baseY: number) {
+
+            // alte löschen
+            for (let s of this.iconSprites)
+                s.destroy()
+
+            this.iconSprites = []
+
+            if (this.type != DisplayType.Icons)
+                return
+
+            for (let i = 0; i < this.value; i++) {
+                let s = sprites.create(this.icon.image.clone(), SpriteKind.UI)
+                s.setFlag(SpriteFlag.RelativeToCamera, true)
+                s.setPosition(baseX + 20 + (i * 12), baseY)
+                this.iconSprites.push(s)
+            }
         }
     }
+
+    // Block Funktionen
+
+    //% block="create cockpit element with icon $img as $type"
+    export function createElement(img: Image, type: DisplayType): CockpitElement {
+        return new CockpitElement(img, type)
+    }
+
+    //% block="set element position x $x y $y"
+    export function setPosition(element: CockpitElement, x: number, y: number) {
+        element.setPosition(x, y)
+    }
+
+    //% block="set element value to $value"
+    export function setValue(element: CockpitElement, value: number) {
+        element.setValue(value)
+    }
+
+    //% block="set element max to $max"
+    export function setMax(element: CockpitElement, max: number) {
+        element.setMax(max)
+    }
 }
- 
